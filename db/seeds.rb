9dad23777557e4aa777182726c10a983d21fb430
Cristina Costa <cncosta25@gmail.com>
2016-06-01 10:47:33 -0500

    seeds so far

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+usernames = ["cristina", "raghu"]
+
+usernames.each do |username|
+  user = User.create
+  user.username = username
+  user.email = "#{username}@example.com"
+  user.password = "12341234"
+  user.save
+end
+
+puts "There are now #{User.count} users in the database."
+
+2.times do
+  family = Family.new
+  family.family_id = family.id
+  family.name = Faker::Name.last
